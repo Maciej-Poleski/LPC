497f1f18c0d93b1fdcf7d83a4d16d88f6fdb904e
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2011-08-21 00:27:57 +0200

    Drobne poprawki do CMakeLists.txt - od tej pory można zainstalować aplikację we wskazanym katalogu (-DCMAKE_INSTALL_PREFIX)

--- src/MainWindow.cxx
+++ src/MainWindow.cxx
@@ -7,6 +7,7 @@ MainWindow::MainWindow () throw (std::bad_alloc) : QMainWindow()
 {
     centralWidget = new MainWidget();
     setCentralWidget (centralWidget);
+    resize(500,300);
 }
 
 MainWindow::~MainWindow()
